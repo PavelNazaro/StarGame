8bba0a3c95110e4bec06255eca02db6ac20adb91
pavelnazaro <pavelnazaro@mail.ru>
2019-12-13 15:48:27 +0300

    Задание по установке фона выполено

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -4,23 +4,31 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture backgroundTexture;
+	Sprite backgroundSprite;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		backgroundTexture = new Texture(Gdx.files.internal("280688-1920x1080.jpg"));
+		backgroundSprite = new Sprite(backgroundTexture);
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+//		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+		backgroundSprite.setSize(700, 500);
+		backgroundSprite.draw(batch);
 		batch.draw(img, 0, 0);
 		batch.end();
 	}
@@ -29,5 +37,6 @@ public class StarGame extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		backgroundTexture.dispose();
 	}
 }
--- desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/desktop/DesktopLauncher.java
@@ -1,12 +1,13 @@
 package ru.geekbrains.desktop;
 
-import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
-import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+
 import ru.geekbrains.StarGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new StarGame(), config);
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		new Lwjgl3Application(new StarGame(), config);
 	}
 }
